70fa57c51a2c11cfa519a440c34bc23d328b6678
Yu Sawai <yusawai.null@gmail.com>
2012-03-03 23:17:17 +0900

    merged master

--- feature_extractor.py
+++ feature_extractor.py
@@ -3,6 +3,7 @@ import nltk
 from nltk import pos_tag
 
 class FeatureExtractor(object):
+<<<<<<< HEAD
     def __init__(self, postagged_sent, ppindex, *ARGS):
         self.sents = postagged_sent
 =======
@@ -14,6 +15,7 @@ class FeatureExtractor(object):
         self.ppindex = ppindex
         self.ARGS = ARGS
         self.featuredict = {}
+<<<<<<< HEAD
 
 
     def features(self):
@@ -32,20 +34,34 @@ class FeatureExtractor(object):
             return self.featuredict
 =======
         self.n = 2
+        self.tagger = nltk.pos_tag
 
 
+    def features(self):
         def _ngramfeature(n = 2):
-            ngrams = [(index - self.ppindex, word) for index, word in enumerate(self.sent) 
-            for i, word in ngrams:
-
             _feature = {}
+            ngrams = [(index - self.ppindex, word) for index, word in enumerate(self.sent) 
+                        if index != self.ppindex and index >= self.ppindex - n]
+            for i, word in ngrams:
+                _feature.update({"%d-gram(%d)_%s"%(n, i, word): 1})
+            return _feature
+
+        def _ngramposfeature(n = 2):
+            _feature = {}
+            taggedsent = self.tagger(self.sent)
             posngrams = [(index - self.ppindex, t[1]) for index, t in enumerate(taggedsent) 
                         if index != self.ppindex and index >= self.ppindex - n]
             for i, word in posngrams:
+                _feature.update({"POS_%d-gram(%d)_%s"%(n, i, word): 1})
             return _feature
 
+        try:
             if "pos" in self.ARGS:
+                self.featuredict.update(_ngramposfeature(self.n))
             if "ngram" in self.ARGS:
+                self.featuredict.update(_ngramfeature(self.n))
 
+        finally:
             return self.featuredict
+
 >>>>>>> test
--- prepchecker.py
+++ prepchecker.py
@@ -13,14 +13,9 @@ class PrepChecker(object):
         self.PREPS = ["in", "for", "at", "on", "of", "about", "with", "from", "by", "as", "into"]
         self.PREPTAGS = ["IN"]
         with open(path, "rb") as pkl:
-            self.corpus = pickle.load(pkl)[0:10]
             self.corpus = pickle.load(pkl)[:20]
         self.training_words = [dic["gold_words"] for dic in self.corpus]
         self.test_words = [dic["test_words"] for dic in self.corpus]
-        self.correctionpairs = [dic["correction_pair"] for dic in self.corpus]
-        self.postagged_testdata = self._postag(tagger, self.test_words)
-        self.postagged_trainingdata = self._postag(tagger, self.training_words)
-        self.features = []
         self.correction_pairs = [dic["correction_pair"] for dic in self.corpus]
         self.labellist = [dic["correction_pair"][1] for dic in self.corpus]
         self.ppindexlist = [dic["ppindex"] for dic in self.corpus]
@@ -30,9 +25,8 @@ class PrepChecker(object):
                               self.ppindexlist, self.labellist, self.correction_pairs)
 >>>>>>> test
 
-    def _postag(self, tagger, datalist): 
-        return [tagger(sent) for sent in datalist]
 
+<<<<<<< HEAD
 
     def makefeatures(self, postagged_sents_list): 
         for postagged_sent, ppindex in zip(postagged_sents_list, self.ppindexlist):
@@ -74,6 +68,7 @@ class PrepChecker(object):
 
 def main():
     pc = PrepChecker("packedcorpus.pkl")
+<<<<<<< HEAD
     pc.makefeatures(pc.postagged_trainingdata)
     print pc.features
     print pc.labellist
